943cd02479dd5eead4b3eb32a30ef5b359d216ac
jprosario <joshuaowens011@gmail.com>
2016-10-10 20:52:19 -0400

    move all spotify+bandsintown controller code to job

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,6 @@ class UsersController < ApplicationController
 
   def show
     user = User.find(params[:id])
-    client = SpotifyClient.for(user)
-    @top_artists = user.top_artists(client).uniq
-    @saved_artists = user.saved_artists(client).uniq
-    @album_artists = user.saved_albums_artists(client).uniq
-    @track_artists = user.saved_tracks_artists(client).uniq
-    @playlist_artists = user.playlists(client).uniq
-    all_artists = [@top_artists, @saved_artists, @album_artists, @track_artists, @playlist_artists].flatten.uniq
-    @events = all_artists.collect {|artist| BandsInTownClient.events_for(artist)}.delete_if { |e| e.empty?  }
+    GrabSpotifyArtistsJob.perform_later(user)
   end
 end
--- app/jobs/grab_spotify_artists_job.rb
+++ app/jobs/grab_spotify_artists_job.rb
@@ -1,7 +1,19 @@
 class GrabSpotifyArtistsJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(user)
+    client = SpotifyClient.for(user)
+    @top_artists = user.top_artists(client).uniq
+    @saved_artists = user.saved_artists(client).uniq
+    @album_artists = user.saved_albums_artists(client).uniq
+    @track_artists = user.saved_tracks_artists(client).uniq
+    @playlist_artists = user.playlists(client).uniq
+    all_artists = [@top_artists, @saved_artists, @album_artists, @track_artists, @playlist_artists].flatten.uniq
+    all_artists.each do |artist_name|
+      artist = Artist.find_or_create_by(name: artist_name)
+      user.artists << artist if !user.artists.include? artist
+    end
+    @events = all_artists.collect {|artist| BandsInTownClient.events_for(artist)}.delete_if { |e| e.empty?  }
+    
   end
 end
