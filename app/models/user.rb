52ab2ce0f9aea61d5b02693fc3d872b767852e4a
jprosario <joshuaowens011@gmail.com>
2016-10-10 22:25:15 -0400

    add emails to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,13 @@ class User < ApplicationRecord
       provider: auth_hash[:provider],
       uid: auth_hash[:uid],
       name: auth_hash[:info][:display_name],
+      email: auth_hash[:info][:email],
       # image_url: auth_hash[:info][:images][0][:url],
       rspotify_hash: auth_hash.to_hash
     )
   end
 
   def self.from_oauth(auth_hash)
-    # alternate method; recent styleguides favour .find_by
-    # user = User.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid])
     user = self.find_by(provider: auth_hash[:provider], uid: auth_hash[:uid])
     user || create_from_oauth(auth_hash)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009231521) do
+ActiveRecord::Schema.define(version: 20161011014517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20161009231521) do
     t.string   "location"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "email"
   end
 
   add_foreign_key "user_artists", "artists"
