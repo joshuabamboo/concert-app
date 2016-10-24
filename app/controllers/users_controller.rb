c3c9590f8fa8635ed68949f214d1bb13a28444b3
jprosario <joshuaowens011@gmail.com>
2016-10-23 21:41:58 -0400

    add user authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user
+  skip_before_action :authenticate_user, only: [:new]
+
   def new
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     GrabSpotifyArtistsJob.perform_later(@user)
   end
 end
