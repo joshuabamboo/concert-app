d6ef82f7e2ece450f0431411081e70124f200261
jprosario <joshuaowens011@gmail.com>
2016-10-23 23:11:09 -0400

    email only on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,20 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = current_user
-    GrabSpotifyArtistsJob.perform_later(@user)
+    if current_user.artists.empty?
+      GrabSpotifyArtistsJob.perform_later(current_user)
+    end
+    @artists = current_user.artists
+    @events = current_user.sort_events
+  end
+
+  def update
+    current_user.update!(user_params)
+    redirect_to current_user, notice: "Your concerts are on the way!" 
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email)
   end
 end
