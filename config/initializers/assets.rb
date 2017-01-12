57155fa30456e62f36ea5abe6326532d09cfa24e
jprosario <joshuaowens011@gmail.com>
2017-01-12 16:58:59 -0500

    precompiled assets for real this time. Fingers crossed.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,4 +17,4 @@ Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "st
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif *.mp4 *.css )
+Rails.application.config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif *.mp4 )
