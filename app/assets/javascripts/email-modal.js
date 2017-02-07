b21f41d7868a9fc8d1ebf3f771eb456270f5533b
Devin Burnette <devin.burnette@gmail.com>
2017-02-07 09:02:59 -0500

    just check the path

--- app/assets/javascripts/email-modal.js
+++ app/assets/javascripts/email-modal.js
@@ -3,15 +3,11 @@ $( document ).ready(function() {
     return document.location.href.indexOf('confirm-email') > -1;
   }
 
-  function hasntConfirmedEmail() {
-    return document.cookie.indexOf('concert_app_email') == -1;
-  }
-
   function showEmailModal() {
     $('#js--email-modal').modal();
   }
 
-	if (userIsLoggedIn() && hasntConfirmedEmail()) {
+	if (userIsLoggedIn()) {
     showEmailModal();
 	}
 });
