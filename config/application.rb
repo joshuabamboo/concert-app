b1ee4570c5892487ffc7470adf0df00fb72234ea
jprosario <joshuaowens011@gmail.com>
2017-02-07 22:49:15 -0500

    configure email assets

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,10 @@ module ConcertApp
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :sidekiq
     config.serve_static_assets = true
+    # config.asset_host = 'http://localhost:3000/' #'https://concert-radar.herokuapp.com/'
+    config.action_controller.asset_host = 'https://concert-radar.herokuapp.com/'
+    config.action_mailer.asset_host = 'https://concert-radar.herokuapp.com/'
   end
 end
  Dotenv::Railtie.load
 RSpotify::authenticate(ENV['SPOTIFY_ID'], ENV['SPOTIFY_SECRET'])
-
